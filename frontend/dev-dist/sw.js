b0ec6cd91ab6334f156f7dfc81d8bca85a9c624b
Zenflow <noreply@zencoder.ai>
2026-02-11 09:59:53 +0530

    first commit

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0f998dfe'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.k4c902sj9h"
+    "revision": "0.p2vg126miv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
