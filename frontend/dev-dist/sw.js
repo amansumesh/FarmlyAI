5cce8b85b297bf6addc50aa5214162b8c65c3ab6
Amal Sumesh <amalsumesh2005@gmail.com>
2026-02-11 22:34:21 +0530

    small changes

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0f998dfe'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.p2vg126miv"
+    "revision": "0.65aqn412urk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
